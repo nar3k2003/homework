330c89a4fea4d20afe102380572e6a9310c2e28e
nar3k2003 <grigoryannarek696@gmail.com>
2024-12-07 18:14:02 +0400

    add new functionality (create table) for HomeworkTask2.1

--- HomeworkTask2.1/js/script.js
+++ HomeworkTask2.1/js/script.js
@@ -12,6 +12,7 @@ function openManu() {
 document.getElementById("submit").onclick = () => {
     const name = document.getElementById("name").value.trim();
     const mail = document.getElementById("mail").value.trim();
+    const company = document.getElementById("company").value.trim();
     const comments = document.getElementById("comments").value.trim();
     const address = document.getElementById("address").value.trim();
     const city = document.getElementById("city").value.trim();
@@ -20,6 +21,7 @@ document.getElementById("submit").onclick = () => {
     const phone = document.getElementById("phone").value.trim();
     const fax = document.getElementById("fax").value.trim();
     const robot = document.getElementById("iamnotarobot").checked;
+    const table = document.getElementById("table");
 
     let isValid = true;
 
@@ -37,12 +39,21 @@ document.getElementById("submit").onclick = () => {
         document.getElementById("nameError").classList.remove("activeError");
     }
     
-    if (!mail) {
+
+    if (!mail ) {
         document.getElementById("mailError").classList.add("activeError");
         isValid = false;
-    } else {
+    }else if(mail.search("@")==-1 || mail.search(".")==-1){
+        document.getElementById("mailInvalidError").classList.add("activeError");
+        isValid = false;
+    }else {
         document.getElementById("mailError").classList.remove("activeError");
+        document.getElementById("mailInvalidError").classList.remove("activeError");
     }
+    console.log(mail.search("@"));
+    console.log(mail.search("."));
+    
+
     
     if (!comments) {
         document.getElementById("commentsError").classList.add("activeError");
@@ -51,11 +62,16 @@ document.getElementById("submit").onclick = () => {
         document.getElementById("commentsError").classList.remove("activeError");
     }
 
+    if (!table.getAttribute("acactiveTable") && isValid) {
+        document.getElementById("table").classList.add("activeTable");
+    }
+
+
     if (isValid) {
         const formData = {
             name: name,
             mail: mail,
-            company: comments,
+            company: company,
             address: address,
             city: city,
             state: state,
@@ -65,26 +81,47 @@ document.getElementById("submit").onclick = () => {
             comments: comments,
         };
         console.log(formData);  
+
+        let table = document.getElementById("table");
+        
+        let row = table.insertRow();
+        let nameCell = row.insertCell();
+        let mailCell = row.insertCell();
+        let companyCell = row.insertCell();
+        let addressCell = row.insertCell();
+        let cityCell = row.insertCell();
+        let stateCell = row.insertCell();
+        let codeCell = row.insertCell();
+        let phoneCell = row.insertCell();
+        let faxCell = row.insertCell();
+        let comentsCell = row.insertCell();
+        let deleteCell = row.insertCell();
+        let editCell = row.insertCell();
+
+        nameCell.innerHTML = formData.name;
+        mailCell.innerHTML = formData.mail;
+        companyCell.innerHTML = formData.company;
+        addressCell.innerHTML = formData.address;
+        cityCell.innerHTML = formData.city;
+        stateCell.innerHTML = formData.state;
+        codeCell.innerHTML = formData.postal;
+        phoneCell.innerHTML = formData.phone;
+        faxCell.innerHTML = formData.fax;
+        comentsCell.innerHTML = formData.comments;
+        deleteCell.innerHTML = "<button id='btnEdit';>Delete</button>";
+        editCell.innerHTML = "<button id='btnEdit';>Edit</button>";
+
+        document.body.appendChild(table);
+    
+        console.log(Object.keys(formData).length);
     }else {
         console.log("Please fill in the required fields.");
     }
 
-
 }
 
-document.getElementById("clearForm").onclick = () => {
-    const inputs = document.querySelectorAll("input, textarea");
-    
-    inputs.forEach(input => {
-        input.value = "";
-    });
 
-    const errorMessages = document.querySelectorAll(".activeError");
-    errorMessages.forEach(error => {
-        error.classList.remove("activeError");
-    });
 
-    console.log("Form cleared.");
-}
+
 
 
